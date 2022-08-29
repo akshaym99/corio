a535f848f75fe69256466885b5048426a97903dc
akshaym99 <akshay.s.mankar@seagate.com>
2022-08-29 15:34:02 +0530

    bucket name lower
    Signed-off-by: akshaym99 <akshay.s.mankar@seagate.com>

--- scripts/s3/s3api/copy_object.py
+++ scripts/s3/s3api/copy_object.py
@@ -60,21 +60,19 @@ class TestS3CopyObjects(S3Api):
             self.finish_time = datetime.now() + timedelta(hours=int(100 * 24))
 
     @classmethod
-    def initialize_variables(cls, test_id, sid):
+    def initialize_variables(cls, test_id):
         """Initialize variables for copy object operations."""
         cls.bucket_name1 = f'copy-obj-bucket1-{test_id}-{perf_counter_ns()}'.lower()
-        cls.bucket_name2 = f"copy-obj-bucket2-{test_id}-{sid}-{perf_counter_ns()}"
-        cls.object_name1 = f"copy-object1-{test_id}-{sid}-{perf_counter_ns()}"
-        cls.object_name2 = f"copy-object2-{test_id}-{sid}-{perf_counter_ns()}"
+        cls.bucket_name2 = f'copy-obj-bucket2-{test_id}-{perf_counter_ns()}'.lower()
+        cls.object_name1 = f'copy-object1-{test_id}-{perf_counter_ns()}'.lower()
+        cls.object_name2 = f'copy-object2-{test_id}-{perf_counter_ns()}'.lower()
 
     # pylint: disable=broad-except
     async def execute_copy_object_workload(self):
         """Execute copy object workload for specific duration."""
-        self.initialize_variables(self.test_id, self.session_id)
-        self.log.info("Creating bucket1 %s", self.bucket_name1)
+        self.initialize_variables(self.test_id)
         await self.create_bucket(self.bucket_name1)
         self.log.info("Created bucket %s", self.bucket_name1)
-        self.log.info("Creating bucket2 %s", self.bucket_name2)
         await self.create_bucket(self.bucket_name2)
         self.log.info("Created bucket %s", self.bucket_name2)
         while True:
